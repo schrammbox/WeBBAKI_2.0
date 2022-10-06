e689403cba85a72c98646531696a3ba7de2db0f4
schrammbox <schrammbox@gmail.com>
2022-10-06 13:27:32 +0200

    Enabling/disabling user works.
    Started on changing branches

--- WeBBAKI/src/main/java/de/thb/webbaki/controller/OfficeController.java
+++ WeBBAKI/src/main/java/de/thb/webbaki/controller/OfficeController.java
@@ -1,5 +1,6 @@
 package de.thb.webbaki.controller;
 
+import de.thb.webbaki.controller.form.BranchForm;
 import de.thb.webbaki.controller.form.UserForm;
 import de.thb.webbaki.entity.User;
 import de.thb.webbaki.repository.UserRepository;
@@ -25,18 +26,24 @@ public class OfficeController {
         final var users = userService.getAllUsers();
 
         UserForm form = new UserForm();
+        String branche = form.getBranche();
+
         form.setUsers(users);
 
         model.addAttribute("form", form);
         model.addAttribute("users", users);
+        model.addAttribute("branche", form);
+
 
         return "permissions/office";
     }
 
     @PostMapping("/office")
     public String deactivateUser(@ModelAttribute("form") @Valid UserForm form){
+        System.out.println(form.getUsers());
 
         userService.deactivateUser(form);
+        userService.changeBranche(form);
 
         return "redirect:office";
     }
--- WeBBAKI/src/main/java/de/thb/webbaki/service/UserService.java
+++ WeBBAKI/src/main/java/de/thb/webbaki/service/UserService.java
@@ -1,5 +1,6 @@
 package de.thb.webbaki.service;
 
+import de.thb.webbaki.controller.form.BranchForm;
 import de.thb.webbaki.controller.form.UserForm;
 import de.thb.webbaki.controller.form.UserRegisterFormModel;
 import de.thb.webbaki.controller.form.UserToRoleFormModel;
@@ -40,7 +41,7 @@ public class UserService {
         return (List<User>) userRepository.findAll();
     }
 
-    public Iterable<User> saveAll(List<User> users){
+    public Iterable<User> saveAll(List<User> users) {
         return userRepository.saveAll(users);
     }
 
@@ -100,8 +101,8 @@ public class UserService {
 
             //Email to Superadmin
             emailSender.send("schrammbox@proton.me", buildAdminEmail("Christian", adminLink,
-                                                                                form.getFirstname(), form.getLastname(),
-                                                                                form.getEmail(), form.getBranche(), form.getCompany()));
+                    form.getFirstname(), form.getLastname(),
+                    form.getEmail(), form.getBranche(), form.getCompany()));
 
             //Email to Superadmin
             emailSender.send("schoenbe@th-brandenburg.de", buildAdminEmail("Leon", adminLink,
@@ -140,7 +141,7 @@ public class UserService {
      * User is enabled if user_confirmation && admin_confirmation == TRUE
      *
      * @param username to get the user
-     * @param token to get the according token
+     * @param token    to get the according token
      * @return value TRUE or FALSE based on INTEGER value (0 = false, 1 = true)
      */
     public int enableUser(String username, String token) {
@@ -217,17 +218,6 @@ public class UserService {
         }
     }
 
-    public void deactivateUser(UserForm userForm){
-        List<User> users = userForm.getUsers();
-
-        for (int i = 0; i < users.size(); i++){
-            if(!Objects.equals(users.get(i), 1)){
-                User user = userRepository.findById(i).get();
-                user.setEnabled(!user.isEnabled());
-            }
-        }
-    }
-
     /**
      * USED IN SUPERADMIN DASHBOARD
      * Superadmin can delete Roles to specific Users
@@ -247,6 +237,31 @@ public class UserService {
         }
     }
 
+    /**
+     * Enable/Disable user to give access to WebBakI
+     *
+     * @param form to get userlist
+     */
+    public void deactivateUser(UserForm form) {
+
+        List<User> users = getAllUsers();
+
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).isEnabled() != (form.getUsers().get(i).isEnabled())) {
+                users.get(i).setEnabled(form.getUsers().get(i).isEnabled());
+            }
+        }
+    }
+
+    public void changeBranche(UserForm form) {
+        List<User> users = getAllUsers();
+
+        for (int i = 0; i < users.size(); i++){
+            if (!Objects.equals(users.get(i).getBranche(), form.getUsers().get(i).getBranche())){
+                users.get(i).setBranche(form.getUsers().get(i).getBranche());
+            }
+        }
+    }
 
 
     private String buildAdminEmail(String name, String link, String userFirstname, String userLastname,
@@ -305,35 +320,35 @@ public class UserService {
                 "\n" +
                 "  <body>\n" +
                 "    <h2 style=\"background-color:black; color: white; padding: 20px 0; margin: 0 auto;\">Neue Registrierung auf WebBaKI</h2>\n" +
-                "    <p>Hallo "+name+",</p>\n" +
+                "    <p>Hallo " + name + ",</p>\n" +
                 "    <p>Es hat sich ein neuer WebBaKI-Nutzer registriert. Infos zum Nutzer:</p>\n" +
                 "    <div class=\"tabledata\" style=\"display:flex;align-items:center; justify-content:center\">\n" +
                 "      <table style=\"\">\n" +
                 "          <tr>\n" +
                 "            <td>Vorname</td>\n" +
-                "            <td>"+userFirstname+"</td>\n" +
+                "            <td>" + userFirstname + "</td>\n" +
                 "          </tr>\n" +
                 "          <tr>\n" +
                 "            <td>Nachname</td>\n" +
-                "            <td>"+userLastname+"</td>\n" +
+                "            <td>" + userLastname + "</td>\n" +
                 "          </tr>\n" +
                 "          <tr>\n" +
                 "            <td>Firma</td>\n" +
-                "            <td>"+userCompany+"</td>\n" +
+                "            <td>" + userCompany + "</td>\n" +
                 "          </tr>\n" +
                 "          <tr>\n" +
                 "            <td>Email</td>\n" +
-                "            <td>"+userEmail+"</td>\n" +
+                "            <td>" + userEmail + "</td>\n" +
                 "          </tr>\n" +
                 "          <tr>\n" +
                 "            <td>Branche</td>\n" +
-                "            <td>"+userBranche+"</td>\n" +
+                "            <td>" + userBranche + "</td>\n" +
                 "          </tr>\n" +
                 "      </table>\n" +
                 "    </div>\n" +
                 "    <p>Der Account kann unter folgendem Link aktiviert oder abgelehnt werden:</p>\n" +
                 "      <p>\n" +
-                "        <a href="+link+">Nutzer verifizieren</a>\n" +
+                "        <a href=" + link + ">Nutzer verifizieren</a>\n" +
                 "        <span></span>\n" +
                 "        <a href=\"http://localhost:8080/confirmation/userDenied\">Nutzer ablehnen</a>\n" +
                 "      </p>\n" +
@@ -398,10 +413,10 @@ public class UserService {
                 "\n" +
                 "  <body>\n" +
                 "    <h2 style=\"background-color:black; color: white; padding: 20px 0; margin: 0 auto;\">Neue Registrierung auf WebBaKI</h2>\n" +
-                "    <p>Hallo "+name+",</p>\n" +
+                "    <p>Hallo " + name + ",</p>\n" +
                 "    <p>Vielen Dank für die Registrierung. Bitte bestätige deine Email unter folgendem Link:</p>\n" +
                 "      <p>\n" +
-                "        <a href="+link+">Verifizieren</a>\n" +
+                "        <a href=" + link + ">Verifizieren</a>\n" +
                 "        <span></span>\n" +
                 "      </p>\n" +
                 "    <p>Der Link bleibt 3 Tage gültig.</p>\n" +
