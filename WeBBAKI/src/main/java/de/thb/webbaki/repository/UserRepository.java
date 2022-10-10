30dae8584a27ef75167fcf6513e93c06dba4e643
schrammbox <schrammbox@gmail.com>
2022-10-10 17:29:26 +0200

    Merged Master & dev/createReport

--- WeBBAKI/src/main/java/de/thb/webbaki/repository/UserRepository.java
+++ WeBBAKI/src/main/java/de/thb/webbaki/repository/UserRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.RepositoryDefinition;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @RepositoryDefinition(domainClass = User.class, idClass = Long.class)
