5912f43b9563e1ce5a15864c5ed1620b91d830e9
sxBUTCHxs <leonschoenberg@gmx.de>
2022-10-18 08:17:07 +0200

    Adds the layer of color in the threatmatrix.

--- WeBBAKI/src/main/java/de/thb/webbaki/service/helper/Counter.java
+++ WeBBAKI/src/main/java/de/thb/webbaki/service/helper/Counter.java
@@ -1,5 +1,9 @@
 package de.thb.webbaki.service.helper;
 
+/**
+ * Is a helper for counting up an Integervalue from 0
+ * Used in the templates
+ */
 public class Counter {
     private int value = 0;
 
