3da9407107cf0ea5a2dcb939964a1b45583a656c
sxBUTCHxs <leonschoenberg@gmx.de>
2022-12-12 17:25:58 +0100

    Add the styling to the js code.

--- WeBBAKI/src/main/resources/static/js/scenarios_script.js
+++ WeBBAKI/src/main/resources/static/js/scenarios_script.js
@@ -1,41 +1,39 @@
 var scenarioString = "<div class=\"scenario\" id=\"scenario{masterScenarioIndex}_{scenarioIndex}\">\n" +
     "              <input hidden=\"\" id=\"masterScenarios{masterScenarioIndex}.scenarios{scenarioIndex}.id\" name=\"masterScenarios[{masterScenarioIndex}].scenarios[{scenarioIndex}].id\" value=\"{scenarioId}\">\n" +
-    "              <textarea placeholder=\"Szenarioname...\" class=\"scenarioName\" id=\"masterScenarios{masterScenarioIndex}.scenarios{scenarioIndex}.name\" name=\"masterScenarios[{masterScenarioIndex}].scenarios[{scenarioIndex}].name\"></textarea>\n" +
-    "              <textarea placeholder=\"Szenariobeschreibung...\" class=\"scenarioDescription\" id=\"masterScenarios{masterScenarioIndex}.scenarios{scenarioIndex}.description\" name=\"masterScenarios[{masterScenarioIndex}].scenarios[{scenarioIndex}].description\"></textarea>\n" +
-    "              <div>\n" +
-    "                <button type=\"button\" value=\"{masterScenarioIndex};{scenarioIndex}\" onclick=\"deleteScenario(this)\">Löschen</button>\n" +
+    "              <textarea placeholder=\"Szenarioname...\" class=\"form-control scenarioName\" id=\"masterScenarios{masterScenarioIndex}.scenarios{scenarioIndex}.name\" name=\"masterScenarios[{masterScenarioIndex}].scenarios[{scenarioIndex}].name\"></textarea>\n" +
+    "              <textarea placeholder=\"Szenariobeschreibung...\" class=\"form-control scenarioDescription\" id=\"masterScenarios{masterScenarioIndex}.scenarios{scenarioIndex}.description\" name=\"masterScenarios[{masterScenarioIndex}].scenarios[{scenarioIndex}].description\"></textarea>\n" +
+    "              <div class=\"deleteScenario\">\n" +
+    "                <button class=\"btn btn-outline-danger\" type=\"button\" value=\"{masterScenarioIndex};{scenarioIndex}\" onclick=\"deleteScenario(this)\">Löschen</button>\n" +
     "              </div>\n" +
     "            </div>"
 
 var masterScenarioString = "<div class=\"masterScenario\" id=\"masterScenario{masterScenarioIndex}\">\n" +
     "          <div class=\"masterScenarioAttributes\">\n" +
     "            <input hidden=\"\" id=\"masterScenarios{masterScenarioIndex}.id\" name=\"masterScenarios[{masterScenarioIndex}].id\" value=\"{masterScenarioId}\">\n" +
-    "            <textarea placeholder=\"Masterszenarioname...\" class=\"masterScenarioName\" id=\"masterScenarios{masterScenarioIndex}.name\" name=\"masterScenarios[{masterScenarioIndex}].name\"></textarea>\n" +
-    "            <div>\n" +
-    "              <select id=\"masterScenarios{masterScenarioIndex}.layer\" name=\"masterScenarios[{masterScenarioIndex}].layer\">\n" +
+    "            <textarea placeholder=\"Masterszenarioname...\" class=\"form-control masterScenarioName\" id=\"masterScenarios{masterScenarioIndex}.name\" name=\"masterScenarios[{masterScenarioIndex}].name\"></textarea>\n" +
+    "            <div class=\"select-masterScenarioName\">\n" +
+    "              <select class=\"form-select\" id=\"masterScenarios{masterScenarioIndex}.layer\" name=\"masterScenarios[{masterScenarioIndex}].layer\">\n" +
     "                <option value=\"1\">Kritische Dienstleistung</option>\n" +
     "                <option value=\"2\">Zugriff</option>\n" +
     "                <option value=\"3\" >Zugang</option>\n" +
     "                <option value=\"4\">Zutritt</option>\n" +
     "                <option value=\"5\" selected=\"selected\">Außenwelt</option>\n" +
     "              </select>\n" +
-    "            </div>\n" +
-    "            <div>\n" +
-    "              <button type=\"button\" value=\"{masterScenarioIndex}\" onclick=\"deleteMasterScenario(this)\">Löschen</button>\n" +
+    "              <button class=\"deleteScenario btn btn-outline-danger\" type=\"button\" value=\"{masterScenarioIndex}\" onclick=\"deleteMasterScenario(this)\">Löschen</button>\n" +
     "            </div>\n" +
     "          </div>\n" +
     "          <div class=\"scenarios\">\n" +
     "            <div class=\"scenario\" id=\"scenario{masterScenarioIndex}_0\">\n" +
     "              <input hidden=\"\" id=\"masterScenarios{masterScenarioIndex}.scenarios0.id\" name=\"masterScenarios[{masterScenarioIndex}].scenarios[0].id\" value=\"-1\">\n" +
-    "              <textarea placeholder=\"Szenarioname...\" class=\"scenarioName\" id=\"masterScenarios{masterScenarioIndex}.scenarios0.name\" name=\"masterScenarios[{masterScenarioIndex}].scenarios[0].name\"></textarea>\n" +
-    "              <textarea placeholder=\"Szenariobeschreibung...\" class=\"scenarioDescription\" id=\"masterScenarios{masterScenarioIndex}.scenarios0.description\" name=\"masterScenarios[{masterScenarioIndex}].scenarios[0].description\">" +
+    "              <textarea placeholder=\"Szenarioname...\" class=\"form-control scenarioName\" id=\"masterScenarios{masterScenarioIndex}.scenarios0.name\" name=\"masterScenarios[{masterScenarioIndex}].scenarios[0].name\"></textarea>\n" +
+    "              <textarea placeholder=\"Szenariobeschreibung...\" class=\"form-control scenarioDescription\" id=\"masterScenarios{masterScenarioIndex}.scenarios0.description\" name=\"masterScenarios[{masterScenarioIndex}].scenarios[0].description\">" +
     "</textarea>\n" +
-    "              <div>\n" +
-    "                <button type=\"button\" value=\"{masterScenarioIndex};0\" onclick=\"deleteScenario(this)\">Löschen</button>\n" +
+    "              <div class=\"deleteScenario\">\n" +
+    "                <button class=\"btn btn-outline-danger\" type=\"button\" value=\"{masterScenarioIndex};0\" onclick=\"deleteScenario(this)\">Löschen</button>\n" +
     "              </div>\n" +
     "            </div>\n" +
     "            \n" +
-    "            <button onclick=\"createScenario(this)\" type=\"button\" class=\"addScenarioButton\" value=\"{masterScenarioIndex};0\">Scenario hinzufügen</button>\n" +
+    "            <button onclick=\"createScenario(this)\" type=\"button\" class=\"submitScenario btn btn-outline-primary addScenarioButton\" value=\"{masterScenarioIndex};0\">Scenario hinzufügen</button>\n" +
     "          </div>\n" +
     "        </div>"
 
