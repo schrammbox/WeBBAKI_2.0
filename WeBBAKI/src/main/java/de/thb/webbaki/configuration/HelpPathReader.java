74e7325fe71f7a661fe007a56f88169fae13aa8b
sxBUTCHxs <leonschoenberg@gmx.de>
2023-02-22 15:53:11 +0100

    Add help path to application.properties

--- WeBBAKI/src/main/java/de/thb/webbaki/configuration/HelpPathReader.java
+++ WeBBAKI/src/main/java/de/thb/webbaki/configuration/HelpPathReader.java
@@ -3,6 +3,9 @@ package de.thb.webbaki.configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+/**
+ * Class for defining path of help.pdf in the application.properties
+ */
 @Component
 @ConfigurationProperties("webbaki.help")
 public class HelpPathReader {
