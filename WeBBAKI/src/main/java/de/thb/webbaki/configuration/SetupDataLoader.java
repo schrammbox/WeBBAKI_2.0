36869ee1251f315789ba0c22a2fe334834b0035f
sxBUTCHxs <leonschoenberg@gmx.de>
2023-03-09 14:16:55 +0100

    delete default_user

--- WeBBAKI/src/main/java/de/thb/webbaki/configuration/SetupDataLoader.java
+++ WeBBAKI/src/main/java/de/thb/webbaki/configuration/SetupDataLoader.java
@@ -56,12 +56,6 @@ public class SetupDataLoader implements
 
         final Role adminRole = createRoleIfNotFound("ROLE_SUPERADMIN", adminPrivileges);
 
-        createUserIfNotFound("Schramm", "Christian", "Geschäftsstelle",
-                "Webbaki", "Passwort1234", Collections.singletonList(adminRole), "schrammbox@gmail.com", true, "schrammbox");
-
-        createUserIfNotFound("Schönberg", "Leon",  "Geschäftsstelle",
-                "Webbaki", "Passwort1234", Collections.singletonList(adminRole), "schoenbe@th-brandenburg.de", true, "schoenbe");
-
         alreadySetup = true;
 
     }
