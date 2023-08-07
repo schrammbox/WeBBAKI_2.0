0b4f8e3829fdd70a42f2c99754a194a775ad7a94
TxRuX <maharneu@gmail.com>
2023-08-07 14:38:02 +0200

    Anfang vom Vergleich

--- WeBBAKI/src/main/java/de/thb/webbaki/controller/ThreatMatrixController.java
+++ WeBBAKI/src/main/java/de/thb/webbaki/controller/ThreatMatrixController.java
@@ -1,5 +1,6 @@
 package de.thb.webbaki.controller;
 
+import com.lowagie.text.DocumentException;
 import de.thb.webbaki.controller.form.ThreatMatrixFormModel;
 import de.thb.webbaki.entity.questionnaire.Questionnaire;
 import de.thb.webbaki.entity.User;
@@ -19,12 +20,17 @@ import de.thb.webbaki.service.snapshot.ReportService;
 import de.thb.webbaki.service.snapshot.SnapshotService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.server.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.context.Context;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+import java.io.IOException;
 
 @Controller
 @AllArgsConstructor
@@ -124,6 +130,77 @@ public class ThreatMatrixController {
         model.addAttribute("nationalReport", nationalReport);
 
         return "threatmatrix/horizontal_vertical_comparison";
+    }
+
+    @GetMapping("threatmatrix/horizontal_vertical_comparison/download")
+    public void downloadReportPdf(HttpServletResponse response, Authentication authentication, HttpServletRequest request) throws UnknownReportFocusException, IOException, DocumentException {
+
+        response.setContentType("application/pdf");
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=horizontaler-vertikaler-vergleich.pdf";
+        response.setHeader(headerKey, headerValue);
+        Context context = new Context();
+        final var masterScenarioList = masterScenarioService.getAllByActiveTrueOrderByPositionInRow();
+        context.setVariable("masterScenarioList",masterScenarioList);
+
+        Snapshot newestSnapshot = snapshotService.getNewestSnapshot()
+                .orElseThrow(() -> new SnapshotNotFoundException("snapshot was not found with the given id"));
+
+        //final var reportList = reportService.getReportBySnapshotId(newestSnapshot.getId());
+
+        //model.addAttribute("reportList", reportList);
+
+        MappingReport companyReport = reportService.getMappingReportByReportFocus(ReportFocus.COMPANY, authentication.getName(), newestSnapshot);
+        context.setVariable("companyReport", companyReport);
+
+        MappingReport branchReport = reportService.getMappingReportByReportFocus(ReportFocus.BRANCHE, authentication.getName(), newestSnapshot);
+        context.setVariable("branchReport", branchReport);
+
+        MappingReport sectorReport = reportService.getMappingReportByReportFocus(ReportFocus.SECTOR, authentication.getName(), newestSnapshot);
+        context.setVariable("sectorReport", sectorReport);
+
+        MappingReport nationalReport = reportService.getMappingReportByReportFocus(ReportFocus.NATIONAL, authentication.getName(), newestSnapshot);
+        context.setVariable("nationalReport", nationalReport);
+
+
+        /*
+        ReportFocus reportFocus = ReportFocus.getReportFocusByEnglishRepresentation(reportFocusString);
+        context.setVariable("reportFocus", reportFocus);
+        */
+
+        /*
+        Snapshot currentSnapshot = snapshotService.getSnapshotByID(snapId).get();
+        context.setVariable("currentSnapshot", currentSnapshot);
+         */
+
+        /*
+        MappingReport report = reportService.getMappingReportByReportFocus(reportFocus, authentication.getName(), currentSnapshot);
+        context.setVariable("report", report);
+         */
+
+        context.setVariable("counter", new Counter());
+
+        // add a random csrf object for the "reportService.parseThymeleafTemplateToHtml" to not crash
+        CsrfToken csrfToken = new CsrfToken() {
+            @Override
+            public String getHeaderName() {
+                return "csrfHeader";
+            }
+
+            @Override
+            public String getParameterName() {
+                return "csrfParameter";
+            }
+
+            @Override
+            public String getToken() {
+                return "csrfToken";
+            }
+        };
+        context.setVariable("_csrf", csrfToken);
+
+        reportService.generatePdfFromHtml(reportService.parseThymeleafTemplateToHtml("threatmatrix/horizontal_vertical_comparison", context),
+                request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort(), response.getOutputStream());
     }
 
 }
