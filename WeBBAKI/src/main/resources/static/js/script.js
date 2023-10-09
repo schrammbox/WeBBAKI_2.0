9a96c1e2726e98346101c77dadc7636bf6a5bd97
sxBUTCHxs <leonschoenberg@gmx.de>
2023-10-09 14:09:17 +0200

    Warning if something is changed and the user wants to click on a nav link

--- WeBBAKI/src/main/resources/static/js/script.js
+++ WeBBAKI/src/main/resources/static/js/script.js
@@ -1,40 +1,58 @@
 $(document).ready(function () {
 
     //create hover-effect for the report nav element
-    $('#dropdown-div-report').hover(function() {
+    $('#dropdown-div-report').hover(function () {
             $("#dropdown-div-report").addClass("show");
             $("#dropdown-ul-report").addClass("show");
-            $("#dropdown-a-report").attr("aria-expanded","true");
+            $("#dropdown-a-report").attr("aria-expanded", "true");
         },
-        function() {
+        function () {
             $("#dropdown-div-report").removeClass("show");
             $("#dropdown-ul-report").removeClass("show");
-            $("#dropdown-a-report").attr("aria-expanded","false");
+            $("#dropdown-a-report").attr("aria-expanded", "false");
         });
 
     //create hover-effect for the threatmatrix nav element
-    $('#dropdown-div-matrix').hover(function() {
+    $('#dropdown-div-matrix').hover(function () {
             $("#dropdown-div-matrix").addClass("show");
             $("#dropdown-ul-matrix").addClass("show");
-            $("#dropdown-a-matrix").attr("aria-expanded","true");
+            $("#dropdown-a-matrix").attr("aria-expanded", "true");
         },
-        function() {
+        function () {
             $("#dropdown-div-matrix").removeClass("show");
             $("#dropdown-ul-matrix").removeClass("show");
-            $("#dropdown-a-matrix").attr("aria-expanded","false");
+            $("#dropdown-a-matrix").attr("aria-expanded", "false");
         });
 
     //create hover-effect for the admin nav element
-    $('#dropdown-div-admin').hover(function() {
+    $('#dropdown-div-admin').hover(function () {
             $("#dropdown-div-admin").addClass("show");
             $("#dropdown-ul-admin").addClass("show");
-            $("#dropdown-a-admin").attr("aria-expanded","true");
+            $("#dropdown-a-admin").attr("aria-expanded", "true");
         },
-        function() {
+        function () {
             $("#dropdown-div-admin").removeClass("show");
             $("#dropdown-ul-admin").removeClass("show");
-            $("#dropdown-a-admin").attr("aria-expanded","false");
+            $("#dropdown-a-admin").attr("aria-expanded", "false");
         });
+
+    const dataChangedDataElement = document.querySelector("#changed-data");
+
+    document.querySelectorAll("input, select, textarea").forEach(inputElement =>{
+        inputElement.addEventListener("change", evt =>{
+            dataChangedDataElement.dataset.changed = "true";
+        });
+    });
+
+    document.querySelectorAll(".nav-link").forEach(navLink => {
+        navLink.addEventListener("click", evt => {
+            if(dataChangedDataElement.dataset.changed == "true"){
+                if(!confirm("Wenn Sie diese Seite verlassen, dann werden Ihre vorgenommenen Änderungen nicht gespeichert. Wollen Sie trotzdem die Seit ewechseln?")){
+                    evt.preventDefault();
+                }
+            }
+        });
+    });
 
 });
 
